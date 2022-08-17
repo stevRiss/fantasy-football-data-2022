77feb92a01964c83dfcc1195c036e6c41a7aa933
Stephen Risso <stevriss22@gmail.com>
2022-08-17 12:25:21 -0400

    got some player data from cbs sports website using open-uri gem and nokogiri gem

--- data.rb
+++ data.rb
@@ -1,4 +1,17 @@
-# require 'HTTParty'
+# require 'HTTParty' --using HTTParty gem
+# response = HTTParty.get("https://www.fantasypros.com/nfl/rankings/consensus-cheatsheets.php")
+# html = response.body
 
-# response = HTTParty
+# puts html
+
+#using open-uri gem
+require 'open-uri'
+require 'nokogiri'
+html = URI.open("https://www.cbssports.com/fantasy/football/rankings/");
+
+doc = Nokogiri::HTML(html)
+stats = doc.css("div").text
+
+puts stats
+
 
